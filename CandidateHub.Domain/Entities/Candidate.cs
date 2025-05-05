e71cf187ccd359d3f5ac0a12288a72f5db73a66f
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 00:46:06 +0300

    Add the list of TimeIntervals as CallTimesPreference for the candidate

--- CandidateHub.Domain/Entities/Candidate.cs
+++ CandidateHub.Domain/Entities/Candidate.cs
@@ -7,7 +7,7 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string CallTimePreference { get; set; }
+        public IEnumerable<TimeInterval> CallTimesPreference { get; set; }
         public string LinkedInUrl { get; set; }
         public string GitHubUrl { get; set; }
         public string Comments { get; set; }
---
+++ CandidateHub.Domain/Entities/TimeInterval.cs
@@ -0,0 +1,8 @@
+﻿namespace CandidateHub.Domain.Entities
+{
+    public class TimeInterval
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
