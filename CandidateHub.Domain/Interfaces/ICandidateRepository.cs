a527c2efb707b0d91e1e7a4f6b09ff489e1b9703
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 00:28:24 +0300

    Add the ICandidateRepository interface in the domain layer

---
+++ CandidateHub.Domain/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,15 @@
+﻿using CandidateHub.Domain.Entities;
+
+namespace CandidateHub.Domain.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<IEnumerable<Candidate>> GetAllAsync(int page = 1, int pageSize = 10);
+        Task<Candidate> GetByIdAsync(int id);
+        Task<Candidate> GetByEmailAsync(string email);
+        Task<Candidate> CreateAsync(Candidate candidate);
+        Task<Candidate> UpdateAsync(Candidate candidate);
+        Task<bool> IsExistsAsync(string email);
+        Task<int> SaveChangesAsync();
+    }
+}
