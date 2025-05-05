7b2e5f51f8604ca50163beded6cc712a501926f8
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 00:27:30 +0300

    Add BaseEntity & Candidate Entity in the domain layer

---
+++ CandidateHub.Domain/Entities/Candidate.cs
@@ -0,0 +1,15 @@
+﻿namespace CandidateHub.Domain.Entities
+{
+    public class Candidate : BaseEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CallTimePreference { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string GitHubUrl { get; set; }
+        public string Comments { get; set; }
+    }
+}
