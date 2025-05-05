edd8a8973f17baa52cd6e2f9123b8d3ae820c557
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 00:49:29 +0300

    Add ApplicationDbContext & CandidateConfiguration

---
+++ CandidateHub.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CandidateHub.Domain.Entities;
+using CandidateHub.Infrastructure.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateHub.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CandidateConfiguration());
+        }
+    }
+}
---
+++ CandidateHub.Infrastructure/EntityConfiguration/CandidateConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CandidateHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CandidateHub.Infrastructure.EntityConfiguration
+{
+    internal class CandidateConfiguration : IEntityTypeConfiguration<Candidate>
+    {
+        public void Configure(EntityTypeBuilder<Candidate> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.FirstName).IsRequired();
+            builder.Property(e => e.LastName).IsRequired();
+            builder.Property(e => e.Email).IsRequired().HasMaxLength(100);
+            builder.HasIndex(e => e.Email).IsUnique();
+
+            builder.HasMany(e => e.CallTimesPreference)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
