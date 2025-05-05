5e982143b1a55af3a8aa20dd5308f0deb5596e81
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 00:53:39 +0300

    Add an Interceptor to set the CreatedAt & UpdateAt data automatically

---
+++ CandidateHub.Infrastructure/Interceptors/AuditableEntitySaveChangesInterceptor.cs
@@ -0,0 +1,40 @@
+﻿using CandidateHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateHub.Infrastructure.Interceptors
+{
+    public class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateEntities(eventData.Context);
+
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            UpdateEntities(eventData.Context);
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        public void UpdateEntities(DbContext? context)
+        {
+            if (context == null) return;
+
+            foreach (var entry in context.ChangeTracker.Entries<BaseEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+}
