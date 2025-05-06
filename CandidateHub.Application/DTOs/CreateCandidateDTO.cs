ed260729634f128b29ef06271c5dd6cbded9f024
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 09:46:03 +0300

    Add CreateCandidateDTO to the application layer

---
+++ CandidateHub.Application/DTOs/CreateCandidateDTO.cs
@@ -0,0 +1,16 @@
+﻿using CandidateHub.Domain.Entities;
+
+namespace CandidateHub.Application.DTOs
+{
+    public record CreateCandidateDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public IEnumerable<TimeIntervalDTO> CallTimesPreference { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string GitHubUrl { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ CandidateHub.Application/DTOs/TimeIntervalDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CandidateHub.Application.DTOs
+{
+    public record TimeIntervalDTO
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public TimeIntervalDTO(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
