b0c71607e53e173306c60e5429301444c7b7ffef
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 13:20:43 +0300

    Add repository implementation

--- CandidateHub.Domain/Interfaces/ICandidateRepository.cs
+++ CandidateHub.Domain/Interfaces/ICandidateRepository.cs
@@ -4,11 +4,8 @@ namespace CandidateHub.Domain.Interfaces
 {
     public interface ICandidateRepository
     {
-        Task<IEnumerable<Candidate>> GetAllAsync(int page = 1, int pageSize = 10);
-        Task<Candidate> GetByIdAsync(int id);
-        Task<Candidate> GetByEmailAsync(string email);
-        Task<Candidate> CreateAsync(Candidate candidate);
-        Task<Candidate> UpdateAsync(Candidate candidate);
+        void Add(Candidate candidate);
+        Task<Candidate?> GetByEmailAsync(string email);
         Task<bool> IsExistsAsync(string email);
         Task<int> SaveChangesAsync();
     }
---
+++ CandidateHub.Infrastructure/Repositories/CandidateRepository.cs
@@ -0,0 +1,38 @@
+﻿using CandidateHub.Domain.Entities;
+using CandidateHub.Domain.Interfaces;
+using CandidateHub.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CandidateHub.Infrastructure.Repositories
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CandidateRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Candidate candidate)
+        {
+            _context.Add(candidate);
+        }
+
+        public Task<Candidate?> GetByEmailAsync(string email)
+        {
+            return _context.Candidates
+                .FirstOrDefaultAsync(c => c.Email.ToLower() == email.ToLower());
+        }
+
+        public async Task<bool> IsExistsAsync(string email)
+        {
+            return await _context.Candidates.AnyAsync(c => c.Email.ToLower() == email.ToLower());
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
