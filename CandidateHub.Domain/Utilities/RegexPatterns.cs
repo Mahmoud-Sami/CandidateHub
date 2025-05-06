b22b3b9bea53af061100a8645868de4a284f0cde
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 09:41:27 +0300

    Add const regex patterns for validating candidate data

---
+++ CandidateHub.Domain/Utilities/RegexPatterns.cs
@@ -0,0 +1,10 @@
+﻿namespace CandidateHub.Domain.Utilities
+{
+    public static class RegexPatterns
+    {
+        public const string GitHubProfileUrlPattern = @"^(?:https?:\/\/)?(?:www\.)?github\.com\/[A-Za-z0-9-]+\/?$";
+        public const string LinkedInProfileUrlPattern = @"^(?:https?:\/\/)?(?:www\.)?linkedin\.com\/in\/[A-Za-z0-9_-]+\/?$";
+        public const string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        public const string PhoneNumberPattern = @"^\+?\d{1,14}$";
+    }
+}
