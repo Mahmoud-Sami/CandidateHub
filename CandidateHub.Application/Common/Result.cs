1aada8e3a993287f124caa5a32e17c442c99c51d
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 13:20:52 +0300

    Add result pattern

---
+++ CandidateHub.Application/Common/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace CandidateHub.Application.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T Value { get; }
+        public string Message { get; }
+
+        private Result(bool isSuccess, T value, string message)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Message = message;
+        }
+
+        public static Result<T> Success(T value) => new(true, value, "success");
+
+        public static Result<T> Failure(string error) => new(false, default, error);
+    }
+
+}
