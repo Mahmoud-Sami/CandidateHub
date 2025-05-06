d25e374417bfeae3645035604d34b3f9ad912dbd
Mahmoud Sami <mahmoud.sami@originsysglobal.com>
2025-05-06 09:51:01 +0300

    Add Mapping Extenstions layer (class) in the application layer

---
+++ CandidateHub.Application/Mapping/MappingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using CandidateHub.Application.DTOs;
+using CandidateHub.Domain.Entities;
+
+namespace CandidateHub.Application.Mapping
+{
+    internal static class MappingExtensions
+    {
+        public static Candidate ToDomainEntity(this CreateCandidateDTO candidateDTO)
+        {
+            var candidate = Candidate.Create(candidateDTO.FirstName,
+                candidateDTO.LastName,
+                candidateDTO.Email,
+                candidateDTO.PhoneNumber,
+                candidateDTO.LinkedInUrl,
+                candidateDTO.GitHubUrl,
+                candidateDTO.Comments);
+
+
+            foreach (var timeIntervalDTO in candidateDTO.CallTimesPreference)
+            {
+                candidate.AddCallTimePreference(timeIntervalDTO.ToDomainEntity());
+            }
+
+            return candidate;
+        }
+
+        public static TimeInterval ToDomainEntity(this TimeIntervalDTO timeIntervalDTO) => new TimeInterval(timeIntervalDTO.Start, timeIntervalDTO.End);
+    
+    }
+}
--- CandidateHub.Domain/Entities/TimeInterval.cs
+++ CandidateHub.Domain/Entities/TimeInterval.cs
@@ -2,8 +2,17 @@
 {
     public class TimeInterval
     {
-        public int Id { get; set; }
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
+        public int Id { get; private set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public TimeInterval(DateTime start, DateTime end)
+        {
+            if (start >= end)
+                throw new ArgumentException("Start time must be less than end time");
+
+            this.Start = start;
+            this.End = end;
+        }
     }
 }
